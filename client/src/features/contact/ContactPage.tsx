bfe0027fa63d2facd9281df9818d18f0829e8f7c
Henrio Randriasoa <henrio.randriasoa@viseo.com>
2023-01-17 15:26:38 +0100

    redux - stage 1

--- client/src/features/contact/ContactPage.tsx
+++ client/src/features/contact/ContactPage.tsx
@@ -1,6 +1,11 @@
-import { Typography } from "@mui/material";
+import { Button, ButtonGroup, Typography } from "@mui/material";
+import { useSelector } from "react-redux/es/exports";
+import { useDispatch } from "react-redux/es/hooks/useDispatch";
+import { CounterState, DECREMENT_COUNTER, INCREMENT_COUNTER } from "./counterReducer";
 
 export default function ContactPage() {
+    const dispatch = useDispatch();
+    const stateObject: CounterState = useSelector((state: CounterState) => state);
 
     return (
         <>
@@ -12,6 +17,28 @@ export default function ContactPage() {
                 Cras scelerisque, dui ut vehicula iaculis, tortor turpis porttitor odio, vitae pellentesque dui metus vitae diam. Nulla quis aliquam eros. Praesent maximus turpis at est pharetra, vitae auctor orci rhoncus. Ut pellentesque consectetur mollis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam a molestie tortor. Donec tempor enim nisi, sit amet pretium magna convallis at.
                 Donec rhoncus enim orci, quis maximus justo luctus porttitor.
             </p>
+
+            <Typography variant="h4">
+                Test Redux
+            </Typography>
+            <p>
+                Data : {stateObject.data}
+            </p>
+            <p>
+                Title : {stateObject.title}
+            </p>
+            <div>
+                <ButtonGroup>
+                    <Button variant="contained" color="error"
+                        onClick={() => dispatch({ type: DECREMENT_COUNTER })}>
+                        Decrement
+                    </Button>
+                    <Button variant="contained" color="primary"
+                        onClick={() => dispatch({ type: INCREMENT_COUNTER })}>
+                        Increment
+                    </Button>
+                </ButtonGroup>
+            </div>
         </>
 
     );
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -5,6 +5,8 @@ import App from './app/layout/App';
 import '@fontsource/roboto/400.css';
 import { BrowserRouter } from 'react-router-dom';
 import { StoreProvider } from './app/context/context';
+import { configureStore } from './app/store/configureStore';
+import { Provider } from 'react-redux';
 
 
 const root = ReactDOM.createRoot(
@@ -12,11 +14,15 @@ const root = ReactDOM.createRoot(
 );
 
 
+const store = configureStore();
+
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <StoreProvider>
+        <Provider store={store}>
         <App />
+        </Provider>
       </StoreProvider>
     </BrowserRouter>
   </React.StrictMode>
